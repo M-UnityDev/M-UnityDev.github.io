1ed9d4b807fd6c523eca3f934f01e250aa939b2a
M-Unity <121330925+M-UnityDev@users.noreply.github.com>
2024-12-23 06:23:43 +0500

    Fix

---
+++ M-Unity's Weird Website/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "M-Unity Software-M-Unity's Weird Website-0.1.0";
+const contentToCache = [
+    "Build/M-Unity's Weird Website.loader.js",
+    "Build/M-Unity's Weird Website.framework.js.unityweb",
+    "Build/M-Unity's Weird Website.data.unityweb",
+    "Build/M-Unity's Weird Website.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
