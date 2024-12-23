c69baf86d17657293b7484ea63bedb728df5bdb1
M-Unity <121330925+M-UnityDev@users.noreply.github.com>
2024-12-23 08:10:37 +0500

    Bugfix

--- Source Code/M-Unity's Weird Website/Assets/ConsoleDirector.cs
+++ Source Code/M-Unity's Weird Website/Assets/ConsoleDirector.cs
@@ -26,32 +26,37 @@ public class ConsoleDirector : MonoBehaviour
         }
         else if(Console.text.EndsWith("\n"))
         {
-            CommandHandler(Console.text.Replace(TempText, null).TrimEnd("\n".ToCharArray()));
+            CommandHandler(Console.text.Replace(TempText, "").TrimEnd("\n".ToCharArray()));
             UpdateText();
         }
     }
     public void CommandHandler(string Command)
     {
-        if (Command == "echo " + Command.TrimStart("echo ".ToCharArray()) || Command == "echo")
+        if (Command.Length >= 5 && Command == "echo " + Command.Remove(0,5) || Command == "echo")
         {   
-            Console.text += Command.TrimStart("echo ".ToCharArray()) + "\n" + DefaultText;
+            Console.text +=  Command.Remove(0,5) + "\n" + DefaultText;
+            return;
         }
-        else if (Command.Replace(" ", null) == "clear" && Command.TrimStart("clear".ToCharArray()) == "")
+        else if (Command.Length >= 5 && Command.Replace(" ", null) == "clear" && Command.Remove(0,5) == "")
         {
             TempText = DefaultText;
             Console.text = TempText;
+            return;
         }
-        else if (Command.Replace(" ", null) == "fastfetch" && Command.TrimStart("fastfetch".ToCharArray()) == "")
+        else if (Command.Length >= 9 && Command.Replace(" ", null) == "fastfetch" && Command.Remove(0,9) == "")
         {
             Console.text += "\n     <b><color=black>################</color>     OS:</b> M-Unity's Weird Website \n     <b><color=black>##</color><color=red>############</color><color=black>##</color>     Kernel:</b> Unity 6000.0.23f1 \n     <b><color=black>##</color><color=red>##</color><color=black>##</color><color=red>##</color><color=red>##</color><color=black>##</color><color=red>##</color><color=black>##</color>     Shell:</b> mish 0.1 \n     <b><color=black>##</color><color=red>############</color><color=black>##</color>     Terminal:</b> muitty 0.1.0 \n     <b><color=black>##</color><color=red>##</color><color=black>##</color><color=red>##</color><color=red>##</color><color=black>##</color><color=red>##</color><color=black>##</color>     Terminal Font:</b> JetBrainsMonoNFM-Regular (15pt)" + "\n     <b><color=black>##</color><color=red>####</color><color=black>####</color><color=red>####</color><color=black>##</color></b>" + "\n     <b><color=black>##</color><color=red>############</color><color=black>##</color></b>" + "\n     <b><color=black>################</color>\n\n</b>" + DefaultText;
+            return;
         }
         else if (Command.Replace(" ", null) == "")
         {
             Console.text += DefaultText;
+            return;
         }
         else
         {
-            Console.text += "mish: Unknown command or command doesn't have this argument" + "\n" + DefaultText;
+            Console.text += "mish: Unknown command or command doesn't have this argument \n" + DefaultText;
+            return;
         }
     }
     private void UpdateText()
