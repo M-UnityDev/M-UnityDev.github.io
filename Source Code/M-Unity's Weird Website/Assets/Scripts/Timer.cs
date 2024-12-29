54e87514b112446129d452d46842e996a02fd8bc
sasha_mat <sashmat123@gmail.com>
2024-12-29 14:04:00 +0500

    TIMER WTF

---
+++ Source Code/M-Unity's Weird Website/Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Unity.Burst;
+using TMPro;
+[BurstCompile]
+public class Timer : MonoBehaviour
+{
+    private int time = 300;
+    [SerializeField] private TMP_Text timtxt;
+    //[SerializeField] private GameObject drk;
+    private void Awake() => StartCoroutine(nameof(Time));
+    private IEnumerator Time()
+    {
+        while(time !< 0 || time != 0)
+        {
+            yield return new WaitForSeconds(1);
+            time -= 1;
+            timtxt.text = (time/60%60).ToString() + ":" + (time%60 < 10 ? "0" : null) +(time%60).ToString();
+        }
+        //drk.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("[5MBD]Cutscene");
+    }
+}
