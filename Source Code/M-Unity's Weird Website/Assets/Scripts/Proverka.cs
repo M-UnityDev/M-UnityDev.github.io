3c9ca1881337b973915c54e0963b69afa05f9e37
sasha_mat <sashmat123@gmail.com>
2024-12-31 20:52:21 +0500

    ПРОВЕРКА ГОТВООВОВООВ

--- Source Code/M-Unity's Weird Website/Assets/Scripts/Proverka.cs
+++ Source Code/M-Unity's Weird Website/Assets/Scripts/Proverka.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class Proverka : MonoBehaviour
 {
-    [SerializeField] private GameObject krovattrue; 
-    [SerializeField] private GameObject covertrue; 
-    [SerializeField] private GameObject kreslotrue; 
-    [SerializeField] private GameObject polkatrue; 
-
-
+    [SerializeField] private GameObject krovat;
+    [SerializeField] private GameObject creslo; 
+    [SerializeField] private GameObject cover; 
+    [SerializeField] private GameObject polka; 
+    [SerializeField] private GameObject readybutton;
 
     void Update()
     {
-        
+        if (AreAllObjectsVisible())
+        {
+            readybutton.SetActive(true);
+        }
+        else
+        {
+            readybutton.SetActive(false);
+        }
+    }
+
+    private bool AreAllObjectsVisible()
+    {
+        return krovat.activeInHierarchy && creslo.activeInHierarchy && cover.activeInHierarchy && polka.activeInHierarchy;
     }
 }
+
+
