e84c1de4826f63e1173697862334cdac13366f6d
sasha_mat <sashmat123@gmail.com>
2024-12-31 12:23:05 +0500

    юнити поменять материалы
    я не доделал только проверку, через пару часов сделаю

---
+++ Source Code/M-Unity's Weird Website/Assets/Scripts/ObjectSwitcher.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectSwitcher : MonoBehaviour
+{
+    [SerializeField] private GameObject originalObject; 
+    [SerializeField] private GameObject blackVariant; 
+    [SerializeField] private GameObject whiteVariant; 
+    [SerializeField] private Button blackButton; 
+    [SerializeField] private Button whiteButton; 
+
+    private void Start()
+    {
+        blackButton.onClick.AddListener(OnBlackButtonClick);
+        whiteButton.onClick.AddListener(OnWhiteButtonClick);
+        
+        blackVariant.SetActive(false);
+        whiteVariant.SetActive(false);
+        
+        blackButton.gameObject.SetActive(false);
+        whiteButton.gameObject.SetActive(false);
+    }
+
+    public void SetObjectToSwitch(GameObject newObject)
+    {
+        originalObject = newObject;
+        
+        if (originalObject != null)
+        {
+            blackButton.gameObject.SetActive(true);
+            whiteButton.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnBlackButtonClick()
+    {
+        SwitchToVariant(blackVariant);
+    }
+
+    private void OnWhiteButtonClick()
+    {
+        SwitchToVariant(whiteVariant);
+    }
+
+    private void SwitchToVariant(GameObject newVariant)
+    {
+        originalObject.SetActive(false);
+        
+        blackVariant.SetActive(false);
+        whiteVariant.SetActive(false);
+        
+        newVariant.SetActive(true);
+        
+        blackButton.gameObject.SetActive(false);
+        whiteButton.gameObject.SetActive(false);
+    }
+}
+
+
---
+++ Source Code/M-Unity's Weird Website/Assets/Scripts/Proverka.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Proverka : MonoBehaviour
+{
+    [SerializeField] private GameObject krovattrue; 
+    [SerializeField] private GameObject covertrue; 
+    [SerializeField] private GameObject kreslotrue; 
+    [SerializeField] private GameObject polkatrue; 
+
+
+
+    void Update()
+    {
+        
+    }
+}
