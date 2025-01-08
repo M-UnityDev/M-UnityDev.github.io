1aa08742f67331dd3853afc6491b599ff2c7e56a
M-Unity <121330925+M-UnityDev@users.noreply.github.com>
2025-01-08 05:20:56 +0500

    muub rewrite

--- Source Code/M-Unity's Weird Website/Assets/WebsiteBootloader/Scripts/MUUBDirector.cs
+++ Source Code/M-Unity's Weird Website/Assets/WebsiteBootloader/Scripts/MUUBDirector.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using Unity.Burst;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 [BurstCompile]
 public class MUUBDirector : MonoBehaviour
 {
@@ -9,15 +10,41 @@ public class MUUBDirector : MonoBehaviour
     private string TempText = "A";
     [SerializeField] private string WelcomeText;
     [SerializeField] private string DefaultText;
-    [SerializeField] private string OSListText;
+    [SerializeField] private string[] OSList;
+    private string OSListText;
+    private int OSCount;
+    private int CommandHandlerCount;
+    private string TempCommand;
+    private string TempCommandArguments;
     private void Awake()
     {
         Console = GetComponent<TMP_InputField>();
         Console.text = WelcomeText;
         Console.text += DefaultText;
         Console.caretPosition = Console.text.Length;
-        TempText = null;
+        //TempText = null;
         TempText = Console.text;
+        foreach(string OS in OSList)
+        {
+            OSCount++;
+            OSListText += OS.Insert(0, "\n  " + OSCount.ToString() + ". ");
+        }
+        OSListText += "\n ";
+    }
+    private void Update()
+    {
+        if(TempText.Length < Console.text.Length)
+        {
+            if(Console.text.Remove(TempText.Length) != TempText)
+            {
+                Console.text = TempText + Console.text.Remove(0,TempText.Length);
+                Console.caretPosition = TempText.Length;
+            }
+        }
+        if(Console.caretPosition < TempText.Length)
+        {
+            Console.caretPosition = TempText.Length;
+        }
     }
     public void ChangeHandler()
     {
@@ -28,23 +55,73 @@ public class MUUBDirector : MonoBehaviour
         }
         else if(Console.text.EndsWith("\n"))
         {
-            CommandHandler(Console.text.Replace(TempText, "").TrimEnd("\n".ToCharArray()));
+            CommandProcessor(Console.text.Replace(TempText, "").TrimEnd("\n".ToCharArray()));
+            Console.text += "\n" + DefaultText;
             UpdateText();
         }
     }
-    public void CommandHandler(string Command)
+    public void CommandProcessor(string Command)
+    {
+        if (Command.Replace(" ", "") == "") return;
+        CommandHandlerCount = 0;
+        TempCommand = null;
+        TempCommandArguments = null;
+        if (Command.Contains(" ")) 
+        {
+            foreach(char c in Command)
+            { 
+                if (c == ' ') break;
+                CommandHandlerCount+=1;
+            }
+            if (Command.Length <= CommandHandlerCount+1) NoArguments(Command);
+            else
+            {
+                TempCommand = Command.Remove(CommandHandlerCount);
+                TempCommandArguments = Command.Remove(0,CommandHandlerCount+1);
+            }
+        }
+        else 
+            NoArguments(Command);
+        CommandHandler(TempCommand,TempCommandArguments);
+    }
+    private void NoArguments(string Command)
+    {
+        TempCommand = Command;
+        TempCommandArguments = null;
+    }
+    public void CommandHandler(string Command, string Arguments)
+    {
+        switch (Command)
+        {
+            case "echo":
+                Console.text += Arguments;
+                break;
+            case "boot":
+                LoadOS(Arguments);
+                break;
+            case "list":
+                Console.text += OSListText;
+                break;
+            default:
+                Console.text += "muub: Unknown command";
+                break;
+        }
+    }
+    public void OLDCommandHandler(string Command)
     {
         if (Command.Length >= 5 && Command == "echo " + Command.Remove(0,5) || Command == "echo")
         {   
-            Console.text +=  Command.Remove(0,5) + "\n" + DefaultText;
+
+            try {Console.text += Command.Remove(0,5) + "\n" + DefaultText;}
+            finally {Console.text += "\n" + DefaultText;}
             return;
         }
-        else if (Command.Length >= 5 && Command == "boot " + Command.Remove(0,5) || Command == "boot")
+        else if (Command.Length >= 5 && Command == "boot " + Command.Remove(0,5) || Command.Contains("boot"))
         {   
             LoadOS(Command.Remove(0,5).Replace(" ", null));
             return;
         }
-        else if (Command.Length >= 4 && Command.Remove(0,4) == "" || Command == "list")
+        else if (Command.Replace(" ", null) == "list")
         {   
             Console.text +=  OSListText + "\n" + DefaultText;
             return;
@@ -72,8 +149,16 @@ public class MUUBDirector : MonoBehaviour
             case "5MBD":
                 SceneManager.LoadScene("[5MBD]CutScene");
                 break;
+            case "C&BD":
+                Console.text += "muub: C&BD is coming soon";
+                //SceneManager.LoadScene("[C&BD]");
+                break;
+            case "MWW":
+                Console.text += "muub: MWW is coming soon";
+                //SceneManager.LoadScene("[MWW]");
+                break;
             default:
-                Console.text += "muub: Unknown OS \n" + DefaultText;
+                Console.text += "muub: Unknown OS";
                 break;
         }
     }
